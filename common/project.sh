c4dc097f6516e17ae22312c7960bf34b1c0aafa1
Joseph Mullally <jwmullally@gmail.com>
2017-06-30 17:12:29 +0100

    Move common items to seperate folder

--- common/project.sh
+++ common/project.sh
@@ -15,29 +15,11 @@ function create {
     # Sometimes we get OOM on Jenkins with default 512Mi limit
     #oc patch deploymentconfig jenkins -p '{"spec":{"template":{"spec":{"containers":[{"name":"jenkins","resources":{"limits":{"memory":"1Gi"}}}]}}}}'
     oc new-app -f https://raw.githubusercontent.com/OpenShiftDemos/gogs-openshift-docker/master/openshift/gogs-persistent-template.yaml
-
-    oc create -f hello.yaml
 }
 
-function create_repos {
+function create_gogs_user {
     ENDPOINT=`oc get route gogs --template={{.spec.host}}`
     curl "http://$ENDPOINT/user/sign_up" --data 'user_name=developer&password=developer&retype=developer&email=nobody@127.0.0.1'
-    curl "http://developer:developer@$ENDPOINT/api/v1/user/repos" --data "name=hello"
-    push_repos
-}
-
-function push_repos {
-    ENDPOINT=`oc get route gogs --template={{.spec.host}}`
-    pushd examples/hello
-    if [ ! -d ".git" ]; then
-        git init && git add * && git commit -m 'initial commit'
-    fi
-    git push --set-upstream "http://developer:developer@$ENDPOINT/developer/hello.git" master
-    popd
-}
-
-function start_build {
-    oc start-build hello-container-pipeline
 }
 
 function delete {
@@ -48,21 +30,13 @@ case $1 in
     create)
         create
         ;;
-    create_repos)
-        create_repos
-        ;;
-    push_repos)
-        push_repos
-        ;;
-    start_build)
-        start_build
+    create-gogs-user)
+        create_gogs_user
         ;;
     delete)
         delete
         ;;
-    --help)
-        echo "$0 admin|create|create_repos|push_repos|delete"
-        ;;
-    *)
+    --help|*)
+        echo "$0 create|create-gogs-user|delete"
         ;;
 esac
---
+++ single_rpm/example.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -ex
+
+function create {
+    oc project rpm-example
+    oc create -f hello.yaml
+}
+
+function create_repos {
+    ENDPOINT=`oc get route gogs --template={{.spec.host}}`
+    curl "http://developer:developer@$ENDPOINT/api/v1/user/repos" --data "name=hello"
+    pushd repos/hello
+    rm -rf .git
+    git init && git add . && git commit -m 'initial commit'
+    git remote add origin "http://developer:developer@$ENDPOINT/developer/hello.git"
+    git push -u origin master --force
+    popd
+}
+
+function start_build {
+    oc start-build hello-container-pipeline
+}
+
+function delete {
+    oc delete all -l app=hello-container
+}
+
+case $1 in
+    create)
+        create
+        ;;
+    create-repos)
+        create_repos
+        ;;
+    start-build)
+        start_build
+        ;;
+    delete)
+        delete
+        ;;
+    --help|*)
+        echo "$0 create|create-repos|start-build|delete"
+        ;;
+esac
